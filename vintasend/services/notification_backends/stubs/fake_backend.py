18cff898e5c8febfedd1f506d3c05b3a303a316a
Hugo bessa <hugo@vinta.com.br>
2024-12-03 20:40:25 -0300

    Fix fake file backend stub

--- vintasend/services/notification_backends/stubs/fake_backend.py
+++ vintasend/services/notification_backends/stubs/fake_backend.py
@@ -12,10 +12,9 @@ from vintasend.exceptions import NotificationNotFoundError
 class FakeFileBackend(BaseNotificationBackend):
     notifications: list[Notification]
     database_file_name: str
-    backend_kwargs: dict
 
     def __init__(self, database_file_name: str = "notifications.json"):
-        self.backend_kwargs = {"database_file_name": database_file_name}
+        super().__init__(database_file_name=database_file_name)
         self.database_file_name = database_file_name
         try:
             notifications_file = open(self.database_file_name, encoding="utf-8")
